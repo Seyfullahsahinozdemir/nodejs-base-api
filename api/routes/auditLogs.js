bb4f5c617d86ff5eb41570a831e662e6620be04e
Seyfullah Sahin <seyfullahsahinozdemir@gmail.com>
2023-08-04 12:20:33 +0300

    dynamic routing

---
+++ api/routes/auditLogs.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+
+router.get("/", (req, res, next) => {
+  res.json({
+    body: req.body,
+    params: req.params,
+    query: req.query,
+    headers: req.headers,
+  });
+});
+
+module.exports = router;
